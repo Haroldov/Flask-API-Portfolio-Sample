fbc41dac60ad6f03196079585b96c79a4dfca9f2
haroldo velez <hvelez020@gmail.com>
2023-02-21 17:21:26 +0000

    add requirements and add wsgi file

--- app/main.py
+++ app/main.py
@@ -5,6 +5,3 @@ app = Flask(__name__)
 
 # import endpoints
 app.add_url_rule('/user/<username>', 'show_user', show_user)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT"), debug=True)
---
+++ app/wsgi.py
@@ -0,0 +1,4 @@
+from main import app
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT"), debug=True)
